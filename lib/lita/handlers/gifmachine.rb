9d86b6c20c6c17d1d5325ed0e35b3a9c5495b7df
Zsolt Prontvai <prozsolt@gmail.com>
2016-01-28 14:42:50 +0100

    Fixed regexp

--- lib/lita/handlers/gifmachine.rb
+++ lib/lita/handlers/gifmachine.rb
@@ -4,19 +4,19 @@ module Lita
       config :base_url, type: String, required: true
       config :seekrit, type: String, required: true
 
-      route(/^gifmachine (.+?)(?:\|(.+?))?(?:\|(.+?))?$/, :gifmachine, help: {
+      route(/^gifmachine (.+?)(?:\|(.*?))?(?:\|(.*?))?$/, :gifmachine, help: {
         "gifmachine <img> [| <top text> [| <bottom text>]]" => "Posts gif to gifmachine"})
 
       def gifmachine(response)
         r = http.post(
           "#{config.base_url}/gif",
-          url: response.match_data[1],
+          url: response.match_data[1].strip,
           who: response.user.name,
           meme_top: response.match_data[2],
           meme_bottom: response.match_data[3],
           seekrit: config.seekrit
         )
-        
+
         response.reply "#{config.base_url} responded with #{r.status.to_s}"
       end
     end
